a5aae9ea35a952b661cd64583e4674564816de0f
Charizard17 <3723esad@gmail.com>
2020-04-26 23:20:23 +0200

    add & edit & clear funcs added

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,23 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class TodoInput extends Component {
-    render() {
-        return (
-            <div className="card card-body my-3">
-                <form>
-                    <div className="input-group">
-                        <div className="input-group-prepend">
-                            <div className="input-group-text bg-primary text-white">
-                            <i className="fa fa-book" aria-hidden="true" />
-                            </div>
-                        </div>
-                        <input type="text" className="form-control text-capitalize" placeholder="add a todo item" />
-                    </div>
-                    <button type="submit" className="btn btn-block btn-primary mt-3" >add item</button>
-                </form>
+  render() {
+    const { item, handleChange, handleSubmit, editItem } = this.props;
+    return (
+      <div className="card card-body my-3">
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <div className="input-group-text bg-primary text-white">
+                <i className="fa fa-book" aria-hidden="true" />
+              </div>
             </div>
-        )
-    }
+            <input
+              type="text"
+              className="form-control text-capitalize"
+              placeholder="add a todo item"
+              value={item}
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit" className={ editItem ? "btn btn-block btn-success mt-3" : "btn btn-block btn-primary mt-3" }>
+            {editItem ? "edit item" : "add item "}
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default TodoInput;
